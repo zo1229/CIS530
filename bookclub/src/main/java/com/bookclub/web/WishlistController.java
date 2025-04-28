3a38f82743e8d6d97b4fbc958e80b8a5d7f18c46
Alphonso Williams <alpwilliams@my365.bellevue.edu>
2025-04-28 00:17:57 -0400

    Week 7

--- bookclub/src/main/java/com/bookclub/web/WishlistController.java
+++ bookclub/src/main/java/com/bookclub/web/WishlistController.java
@@ -1,8 +1,9 @@
-package main.java.com.bookclub.web;
+package com.bookclub.web;
 
 import com.bookclub.model.WishlistItem;
 import com.bookclub.service.dao.WishlistDao;
-import com.bookclub.service.impl.MemWishlistDao;
+import com.bookclub.service.impl.MongoWishlistDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,38 +11,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @RequestMapping("/wishlist")
 public class WishlistController {
 
+    WishlistDao wishlistDao = new MongoWishlistDao();
+
+    @Autowired
+    private void setWishlistDao(WishlistDao wishlistDao) {
+        this.wishlistDao = wishlistDao;
+    }
+
     @RequestMapping(method = RequestMethod.GET)
-    public String showWishlist(Model model) {
-        MemWishlistDao wishlistDao = new MemWishlistDao();
-
-        List<WishlistDao> wishlist = wishlistDao.list();
-
-        model.addAttribute("wishlist", wishlist);
-
+    public String showWishlist() {
         return "wishlist/list";
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/new")
     public String wishlistForm(Model model) {
-        model.addAttribute("wishlistItem", new WishlistDao());
+        model.addAttribute("wishlistItem", new WishlistItem());
         return "wishlist/new";
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public String addWishlistItem(@Valid WishlistDao wishlistItem, BindingResult bindingResult) {
+    public String addWishlistItem(@Valid WishlistItem wishlistItem, BindingResult bindingResult) {
         System.out.println(wishlistItem.toString());
-
-        System.out.println(bindingResult.getAllErrors());
 
         if (bindingResult.hasErrors()) {
             return "wishlist/new";
         }
+
+        wishlistDao.add(wishlistItem); // add the record to MongoDB
 
         return "redirect:/wishlist";
     }
