4c8f74dcf92f4a4c49ec1fa68df3f040a5d2fa18
Alphonso Williams <alpwilliams@my365.bellevue.edu>
2025-05-04 23:07:58 -0400

    Week 8

--- bookclub/src/main/java/com/bookclub/model/Book.java
+++ bookclub/src/main/java/com/bookclub/model/Book.java
@@ -2,46 +2,72 @@ package com.bookclub.model;
 
 import java.util.List;
 
-public class Book {
+public class Book
+{
     private String isbn;
     private String title;
     private String description;
+    private String infoUrl;
     private int numOfPages;
-    private List<String> authors;
 
     public Book() {}
 
-    public Book(String isbn, String title, String description, int numOfPages, List<String> authors) {
+    public Book(String isbn, String title, String description, String infoUrl, int numOfPages) {
         this.isbn = isbn;
         this.title = title;
         this.description = description;
+        this.infoUrl = infoUrl;
         this.numOfPages = numOfPages;
-        this.authors = authors;
     }
 
-    public String getIsbn() { return isbn; }
-    public void setIsbn(String isbn) { this.isbn = isbn; }
+    public Book(String isbn, String title, String infoUrl) {
+        this.isbn = isbn;
+        this.title = title;
+        this.infoUrl = infoUrl;
+    }
 
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
+    public String getIsbn() {
+        return isbn;
+    }
 
-    public int getNumOfPages() { return numOfPages; }
-    public void setNumOfPages(int numOfPages) { this.numOfPages = numOfPages; }
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-    public List<String> getAuthors() { return authors; }
-    public void setAuthors(List<String> authors) { this.authors = authors; }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setNumOfPages(int numOfPages) {
+        this.numOfPages = numOfPages;
+    }
+
+    public int getNumOfPages() {
+        return numOfPages;
+    }
+
+    public void setInfoUrl(String infoUrl) {
+        this.infoUrl = infoUrl;
+    }
+
+    public String getInfoUrl() {
+        return infoUrl;
+    }
 
     @Override
     public String toString() {
-        return "Book{" +
-                "isbn='" + isbn + '\'' +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", numOfPages=" + numOfPages +
-                ", authors=" + authors +
-                '}';
+        return String.format("Book{isbn=%s, title=%s, description=%s, infoUrl=%s, numOfPages=%s}", isbn, title, description, infoUrl, numOfPages);
     }
 }
